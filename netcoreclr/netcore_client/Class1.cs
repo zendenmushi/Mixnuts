599599676199d8e9b9289e85807504ac263a724e
hzc02016@nifty.ne.jp <zendenmushi>
2019-10-27 00:36:40 +0900

    add demo client

---
+++ netcoreclr/netcore_client/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace netcore_client
+{
+    [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+    public delegate void THostShowMessage([MarshalAs(UnmanagedType.LPStr)]string msg);  
+    public class Class1
+    {
+        // delphi側から呼び出される関数。例として呼び出し元のパスと、コールバック関数ポインタが渡される
+        public static void Entry([MarshalAs(UnmanagedType.LPStr)] string AppPath, IntPtr funcptr)
+        {
+            string msg = "call from "+AppPath;
+            var callback = (THostShowMessage)Marshal.GetDelegateForFunctionPointer(funcptr, typeof(THostShowMessage));
+
+            // 文字列を加工して、コールバック
+            callback(msg);
+
+        }
+
+    }
+}
